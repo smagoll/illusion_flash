f7da61bd0ea816ebe8dacee68e198012902e5152
smagoll <68508178+smagoll@users.noreply.github.com>
2025-07-08 09:59:29 +0500

    added input

--- Assets/Project/Scripts/Gameplay/Player/MovementController.cs
+++ Assets/Project/Scripts/Gameplay/Player/MovementController.cs
@@ -1,16 +1,54 @@
-using Input;
 using UnityEngine;
-using Zenject;
 
 public class MovementController : MonoBehaviour
 {
-    [SerializeField]
-    private CharacterController characterController;
+    [Header("Components")]
+    [SerializeField] private CharacterController characterController;
 
-    
-    
-    public void Move(Vector3 direction)
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float smoothTime = 0.1f;
+
+    [Header("Jump & Gravity")]
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float jumpForce = 5f;
+
+    private Vector3 _currentVelocity; // Для сглаживания движения
+    private Vector3 _smoothDirection; // Текущее сглаженное направление
+    private float _verticalVelocity;  // Вертикальное движение (гравитация/прыжок)
+
+    public void Move(Vector2 inputDirection)
     {
-        characterController.Move(direction);
+        ApplyHorizontalMovement(inputDirection);
+        ApplyGravity();
+
+        Vector3 move = (_smoothDirection * moveSpeed) + Vector3.up * _verticalVelocity;
+        characterController.Move(move * Time.deltaTime);
+    }
+
+    private void ApplyHorizontalMovement(Vector2 input)
+    {
+        Vector3 targetDirection = new Vector3(input.x, 0, input.y).normalized;
+
+        // Сглаживаем движение для мягкости
+        _smoothDirection = Vector3.SmoothDamp(
+            _smoothDirection, 
+            targetDirection, 
+            ref _currentVelocity, 
+            smoothTime
+        );
+    }
+
+    private void ApplyGravity()
+    {
+        if (characterController.isGrounded)
+        {
+            _verticalVelocity = -1f;
+
+        }
+        else
+        {
+            _verticalVelocity += gravity * Time.deltaTime;
+        }
     }
 }
--- Assets/Project/Scripts/Input/InputService.cs
+++ Assets/Project/Scripts/Input/InputService.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Input
 {
@@ -7,19 +8,34 @@ namespace Input
         Vector2 MoveAxis { get; }
         bool JumpPressed { get; }
     }
-    
+
     public class InputService : MonoBehaviour, IInputService
     {
+        private PlayerInput _inputActions;
         private Vector2 _moveAxis;
         private bool _jumpPressed;
 
         public Vector2 MoveAxis => _moveAxis;
         public bool JumpPressed => _jumpPressed;
 
-        private void Update()
+        private void Awake()
         {
-            _moveAxis = new Vector2(UnityEngine.Input.GetAxisRaw("Horizontal"), UnityEngine.Input.GetAxisRaw("Vertical"));
-            _jumpPressed = UnityEngine.Input.GetButtonDown("Jump");
+            _inputActions = new PlayerInput();
+
+            // Привязка событий
+            _inputActions.Player.Move.performed += ctx => _moveAxis = ctx.ReadValue<Vector2>();
+            _inputActions.Player.Move.canceled += _ => _moveAxis = Vector2.zero;
+
+            _inputActions.Player.Jump.performed += _ => _jumpPressed = true;
+        }
+
+        private void OnEnable() => _inputActions.Enable();
+        private void OnDisable() => _inputActions.Disable();
+
+        private void LateUpdate()
+        {
+            // Сброс после обработки
+            _jumpPressed = false;
         }
     }
 }
